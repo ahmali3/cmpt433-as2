1aad2790847146daa22c0800c3e2516b263872f5
ahmali3 <60248545+ahmali3@users.noreply.github.com>
2023-02-04 01:30:20 -0800

    1.5 completed

--- i2c.c
+++ i2c.c
@@ -1,3 +1,4 @@
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,111 +7,102 @@
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 #include <pthread.h>
+#include <stdbool.h>
+#include "i2c.h"
+#include "utility.h"
+#include "sampler.h"
 
-
-#define I2CDRV_LINUX_BUS1 "/dev/i2c-1"
-
-#define I2C_DEVICE_ADDRESS 0x20
-
-#define REG_DIRA 0x00
-#define REG_DIRB 0x01
-#define REG_OUTA 0x14
-#define REG_OUTB 0x15
-
-static int initI2cBus(char* bus, int address);
-static void writeI2cReg(int i2cFileDesc, unsigned char regAddr, unsigned char value);
-static unsigned char readI2cReg(int i2cFileDesc, unsigned char regAddr);
-
-// create the array
+// Arrays of the hex values to write to the top and bottom segments of the 14-segment display
 int topDigits[10] = {0x86, 0x02, 0x0E, 0x06, 0x8A, 0x8C, 0x8C, 0x14, 0x8E, 0x8E};
 int bottomDigits[10] = {0xA1, 0x80, 0x31, 0xB0, 0x90, 0xB0, 0xB1, 0x02, 0xB1, 0x90};
 
-//On the Zen Cape, to display unique patterns (characters) on each of the digits, one must quickly switch 
-// between the two digits. Basically you turn on the left digit and drive the pattern for the left digit. The 
-// switch to turning on just the right digit and drive the pattern for the right digit. If this is done fast 
-// enough then it seems to the human eye that each digit is on all the time.
-// Here is the pseudo code for doing this:
-// 1. Spawn a background thread.
-// 2. In background thread, continuously loop through:
-// 1. Turn off both digits by driving a 0 to GPIO pins (Linux #'s 61 and 44).
-// 2. Drive I2C GPIO extender to display pattern for left digit.
-// 3. Turn on left digit via GPIO 61 set to a 1. Wait for a little time.
-// (Waiting for 5ms seems to work well).
-// 4. Turn off both digits by driving a 0 to GPIO pins (Linux #'s 61 and 44).
-// 5. Drive I2C GPIO extender to display pattern for right digit.
-// 6. Turn on right digit via GPIO 44 set to a 1. Wait a little time.
-// (Waiting for 5ms seems to work well).
+bool displayThreadRunning = false;
 
-// This function is the background thread that does the work of displaying the digits.
+void modifyFile(FILE *file, char *value)
+{
+rewind(file);
+fputs(value, file);
+fflush(file);
+}
+
+
+// Background thread that displays the digits on the 14-segment display
 void *displayDigits(void *arg)
 {
-    int i2cFileDesc = initI2cBus(I2CDRV_LINUX_BUS1, I2C_DEVICE_ADDRESS);
+	int i2cFileDesc = initI2cBus(I2CDRV_LINUX_BUS1, I2C_DEVICE_ADDRESS);
+	initDisplay();
     writeI2cReg(i2cFileDesc, REG_DIRA, 0x00);
     writeI2cReg(i2cFileDesc, REG_DIRB, 0x00);
 
-    while (1)
+	FILE *leftDigitFile = fopen("/sys/class/gpio/gpio61/value", "w");
+	FILE *rightDigitFile = fopen("/sys/class/gpio/gpio44/value", "w");
+	int numDips = 0;
+	int leftDigit = 0;
+	int rightDigit = 0;
+	int timeElapsedInMs = 100;
+
+    while (displayThreadRunning)
     {
-        // Turn off both digits
-        writeI2cReg(i2cFileDesc, REG_OUTA, 0x00);
-        writeI2cReg(i2cFileDesc, REG_OUTB, 0x00);
+		// Set both pins to 0
+		modifyFile(leftDigitFile, TURN_OFF);
+		modifyFile(rightDigitFile, TURN_OFF);
+		if (timeElapsedInMs == 100)
+		{
+			if (Sampler_getNumSamplesInHistory() > 99)
+				numDips = 99;
+			else
+				numDips = Sampler_getNumSamplesInHistory();
 
-        // Display left digit
-        writeI2cReg(i2cFileDesc, REG_OUTA, topDigits[0]);
-        writeI2cReg(i2cFileDesc, REG_OUTB, bottomDigits[0]);
+			leftDigit = numDips / 10;
+			rightDigit = numDips % 10;
+			timeElapsedInMs = 0;
+		}
 
-        // Turn on left digit
-        writeI2cReg(i2cFileDesc, REG_OUTA, 0x40);
+		// Display left digit
+		writeI2cReg(i2cFileDesc, REG_OUTA, bottomDigits[leftDigit]);
+		writeI2cReg(i2cFileDesc, REG_OUTB, topDigits[leftDigit]);
 
-        // Wait a little bit
+		// Turn on left digit
+		modifyFile(leftDigitFile, TURN_ON);
+
         usleep(5000);
 
-        // Turn off both digits
-        writeI2cReg(i2cFileDesc, REG_OUTA, 0x00);
-        writeI2cReg(i2cFileDesc, REG_OUTB, 0x00);
+		// Set both pins to 0
+		modifyFile(leftDigitFile, TURN_OFF);
+		modifyFile(rightDigitFile, TURN_OFF);
 
-        // Display right digit
-        writeI2cReg(i2cFileDesc, REG_OUTA, topDigits[1]);
-        writeI2cReg(i2cFileDesc, REG_OUTB, bottomDigits[1]);
+		// Display right digit
+		writeI2cReg(i2cFileDesc, REG_OUTA, bottomDigits[rightDigit]);
+		writeI2cReg(i2cFileDesc, REG_OUTB, topDigits[rightDigit]);
 
-        // Turn on right digit
-        writeI2cReg(i2cFileDesc, REG_OUTB, 0x10);
+		// Turn on right digit
+		modifyFile(rightDigitFile, TURN_ON);
 
-        // Wait a little bit
-        usleep(5000);
+		usleep(5000);
+		timeElapsedInMs += 10;
     }
+	return NULL;
 }
 
-int main()
+// Begins the background thread which displays the digits
+void startDisplayThread(void)
 {
-    // Create a background thread to display the digits
-    pthread_t thread;
-    pthread_create(&thread, NULL, displayDigits, NULL);
+	if (displayThreadRunning)
+		return;
 
-    // Wait for the background thread to finish
-    pthread_join(thread, NULL);
-
-    return 0;
-
-	// printf("Drive display (assumes GPIO #61 and #44 are output and 1\n");
-	// int i2cFileDesc = initI2cBus(I2CDRV_LINUX_BUS1, I2C_DEVICE_ADDRESS);
-
-	// writeI2cReg(i2cFileDesc, REG_DIRA, 0x00);
-	// writeI2cReg(i2cFileDesc, REG_DIRB, 0x00);
-
-	// // Drive an hour-glass looking character (Like an X with a bar on top & bottom)
-	// writeI2cReg(i2cFileDesc, REG_OUTA, 0x2A);
-	// writeI2cReg(i2cFileDesc, REG_OUTB, 0x54);
-
-	// // Read a register:
-	// unsigned char regVal = readI2cReg(i2cFileDesc, REG_OUTA);
-	// printf("Reg OUT-A = 0x%02x\n", regVal);
-
-	// // Cleanup I2C access;
-	// close(i2cFileDesc);
-	// return 0;
+	displayThreadRunning = true;
+	pthread_t displayThread;
+	pthread_create(&displayThread, NULL, displayDigits, NULL);
+	pthread_detach(displayThread);
 }
 
-static int initI2cBus(char* bus, int address)
+// Stops the background thread which displays the digits
+void stopDisplayThread(void)
+{
+	displayThreadRunning = false;
+}
+
+int initI2cBus(char* bus, int address)
 {
 	int i2cFileDesc = open(bus, O_RDWR);
 	if (i2cFileDesc < 0) {
@@ -127,7 +119,7 @@ static int initI2cBus(char* bus, int address)
 	return i2cFileDesc;
 }
 
-static void writeI2cReg(int i2cFileDesc, unsigned char regAddr, unsigned char value)
+void writeI2cReg(int i2cFileDesc, unsigned char regAddr, unsigned char value)
 {
 	unsigned char buff[2];
 	buff[0] = regAddr;
@@ -139,21 +131,22 @@ static void writeI2cReg(int i2cFileDesc, unsigned char regAddr, unsigned char va
 	}
 }
 
-static unsigned char readI2cReg(int i2cFileDesc, unsigned char regAddr)
+// Initializes the 14-segment i2c display
+void initDisplay(void)
 {
-	// To read a register, must first write the address
-	int res = write(i2cFileDesc, &regAddr, sizeof(regAddr));
-	if (res != sizeof(regAddr)) {
-		perror("Unable to write i2c register.");
-		exit(-1);
-	}
+	// Configure both pins on the microprocessor for output through GPIO.
+    // If GPIO pins not yet exported, then export them (avoid re-exporting pins)	
+	if (system("echo 61 > /sys/class/gpio/export") != 0)
+		system("echo 61 > /sys/class/gpio/export");
 
-	// Now read the value and return it
-	char value = 0;
-	res = read(i2cFileDesc, &value, sizeof(value));
-	if (res != sizeof(value)) {
-		perror("Unable to read i2c register");
-		exit(-1);
-	}
-	return value;
+	if (system("echo 44 > /sys/class/gpio/export") != 0)
+		system("echo 44 > /sys/class/gpio/export");
+
+	// Enable Linux I2C support for Linux I2C bus #1 (HW bus 1)
+	runCommand("config-pin P9_18 i2c");
+	runCommand("config-pin P9_17 i2c");
+
+	// // Direction to output
+	runCommand("echo out > /sys/class/gpio/gpio61/direction");
+	runCommand("echo out > /sys/class/gpio/gpio44/direction");
 }
